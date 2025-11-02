5a9bb202a91b0075434cba31668adc8db147b32c
jaishreeverma2004 <jaishree.verma.work@gmail.com>
2025-11-02 20:10:43 +0530

    Basics of Python

--- Python/typeconversion.py
+++ Python/typeconversion.py
@@ -3,4 +3,5 @@ x = input("x: ")
 # here input comes as a string so error because of type error as "1" != 1 in python
 # so type conversion string to number to get output
 y = int(x)+1
+print(x,y)
 print(f"x:{x},y:{y}")
