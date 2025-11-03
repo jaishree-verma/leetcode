f1380b07baea2f4b502f2fc38fedfd3af6b21f51
jaishreeverma2004 <jaishree.verma.work@gmail.com>
2025-11-03 18:36:51 +0530

    JavaScript Basics

---
+++ JavaScript/Basics/tobeornototbe.js
@@ -0,0 +1,16 @@
+// Example 
+// Input: func = () => expect(5).toBe(5)
+// Output: {"value": true}
+// Explanation: 5 === 5 so this expression returns true.
+var expect = function(val) {
+    return {
+        toBe: function(otherVal) {
+            if (val === otherVal) return true;
+            else throw new Error("Not Equal");
+        },
+        notToBe: function(otherVal) {
+            if (val !== otherVal) return true;
+            else throw new Error("Equal");
+        }
+    };
+};
