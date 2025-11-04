bde6b97528fe9fff150ab82d1b51b9588a9ceffd
jaishreeverma2004 <jaishree.verma.work@gmail.com>
2025-11-04 11:37:01 +0530

    JavaScript Basics

---
+++ JavaScript/Basics/counter2.js
@@ -0,0 +1,24 @@
+// Example 
+// Input: init = 5, calls = ["increment","reset","decrement"]
+// Output: [6,5,4]
+// Explanation:
+// const counter = createCounter(5);
+// counter.increment(); // 6
+// counter.reset(); // 5
+// counter.decrement(); // 4
+
+var createCounter = function(init) {
+    let a= init;
+    return {
+        increment : function(){
+            return ++a;
+        },
+        decrement : function(){
+            return --a;
+        },
+        reset : function(){
+            a = init
+            return a;
+        }
+    }  
+};
