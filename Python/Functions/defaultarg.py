79becb8107a7eaef9643e5679f0167d966b0bf7a
jaishreeverma2004 <jaishree.verma.work@gmail.com>
2025-11-04 14:52:54 +0530

    Basics of Python

--- Python/Functions/defaultarg.py
+++ Python/Functions/defaultarg.py
@@ -12,8 +12,31 @@ print(increment(2, 5))
 # all option parameters should come before required one
 
 
-def increment(number, another_number, by=1):    # optional paaremter added beofre required one means before by 
+# optional paaremter added beofre required one means before by
+def increment(number, another_number, by=1):
     return ((number * another_number) + by)     # any operation
 
 
 print(increment(2, 3, 1))
+
+# or
+
+
+def square(numbers):
+    return numbers*numbers
+
+
+print(square(5))
+
+# or
+
+
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+        total *= number
+    return total
+
+
+# they are tuples which are iterable & used in loops
+print(multiply(2, 3, 4, 5))     # product of these numbers is 120
