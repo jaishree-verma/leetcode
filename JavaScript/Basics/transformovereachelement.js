89000a710e5b8721cfd9705ff166948bda58a224
jaishreeverma2004 <jaishree.verma.work@gmail.com>
2025-11-05 10:23:31 +0530

    JavaScript Basics

---
+++ JavaScript/Basics/transformovereachelement.js
@@ -0,0 +1,14 @@
+// Example 
+// Input: arr = [1,2,3], fn = function plusone(n) { return n + 1; }
+// Output: [2,3,4]
+// Explanation:
+// const newArray = map(arr, plusone); // [2,3,4]
+// The function increases each value in the array by one. 
+
+var map = function(arr, fn) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(fn(arr[i], i));
+    }
+    return result;
+};
