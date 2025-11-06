8222dff6854e8052abbbdbf8a75dc831accfe00d
jaishreeverma2004 <jaishree.verma.work@gmail.com>
2025-11-06 14:59:56 +0530

    JavaScript Basics

---
+++ JavaScript/Basics/arraybasedtransformations.js
@@ -0,0 +1,37 @@
+// Example 
+// Input: 
+// nums = [1,2,3,4]
+// fn = function sum(accum, curr) { return accum + curr; }
+// init = 0
+// Output: 10
+// Explanation:
+// initially, the value is init=0.
+// (0) + nums[0] = 1
+// (1) + nums[1] = 3
+// (3) + nums[2] = 6
+// (6) + nums[3] = 10
+// The final answer is 10.
+
+// Example 
+// Input: 
+// nums = [1,2,3,4]
+// fn = function sum(accum, curr) { return accum + curr * curr; }
+// init = 100
+// Output: 130
+// Explanation:
+// initially, the value is init=100.
+// (100) + nums[0] * nums[0] = 101
+// (101) + nums[1] * nums[1] = 105
+// (105) + nums[2] * nums[2] = 114
+// (114) + nums[3] * nums[3] = 130
+// The final answer is 130.
+
+var reduce = function(nums, fn, init) {
+    let val = init;
+    for(let i=0;i<nums.length;i++){
+       val = fn(val,nums[i])
+    }
+    return val;
+    
+    
+};
