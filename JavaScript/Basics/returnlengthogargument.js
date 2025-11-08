6f4ba03b04aa61d8dd1b1c53ffdb2ddbb58f2ee6
jaishreeverma2004 <jaishree.verma.work@gmail.com>
2025-11-09 00:33:33 +0530

    JavaScript Basics

---
+++ JavaScript/Basics/returnlengthogargument.js
@@ -0,0 +1,15 @@
+// 2703
+// Example 
+// Input: args = [5]
+// Output: 1
+// Explanation:
+// argumentsLength(5); // 1
+// One value was passed to the function so it should return 1.
+
+// Example 
+// Input: args = [{}, null, "3"]
+// Output: 3
+// Explanation: 
+// argumentsLength({}, null, "3"); // 3
+
+// Three values were passed to the function so it should return 3.
