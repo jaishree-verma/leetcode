228c13a7e00d9c129410cacabad7e7f91f87e916
jaishreeverma2004 <jaishree.verma.work@gmail.com>
2025-11-10 21:52:35 +0530

    JavaScript Basics

--- JavaScript/Basics/allowoefunctioncall.js
+++ JavaScript/Basics/allowoefunctioncall.js
@@ -1,1 +1,32 @@
 // 2666
+// Example 
+// // Input: fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6]]
+// Output: [{"calls":1,"value":6}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(1, 2, 3); // 6
+// onceFn(2, 3, 6); // undefined, fn was not called
+
+// Example 
+// Input: fn = (a,b,c) => (a * b * c), calls = [[5,7,4],[2,3,6],[4,6,8]]
+// Output: [{"calls":1,"value":140}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(5, 7, 4); // 140
+// onceFn(2, 3, 6); // undefined, fn was not called
+// onceFn(4, 6, 8); // undefined, fn was not called
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let called = false;
+    
+    return function(...args){
+        if(!called){
+            called = true;
+            return fn(...args);
+        }
+    }
+};
