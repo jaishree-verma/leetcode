83b92827181c15b0f29164d6f6b53ce8d5c514e5
jaishree-verma <jaishree.verma.work@gmail.com>
2025-11-13 09:25:10 +0530

    JavaScript Basics

--- JavaScript/Basics/timeoutcancellation.js
+++ JavaScript/Basics/timeoutcancellation.js
@@ -17,3 +17,13 @@
 // const cancelFn = cancellable((x) => x**2, [2], 100);
 // setTimeout(cancelFn, cancelTimeMs);
 // The cancellation was scheduled to occur after a delay of cancelTimeMs (50ms), which happened before the execution of fn(2) at 100ms, resulting in fn(2) never being called.
+
+// Example 3:
+// Input: fn = (x1, x2) => x1 * x2, args = [2,4], t = 30
+// Output: [{"time": 30, "returned": 8}]
+// Explanation: 
+// const cancelTimeMs = 100;
+// const cancelFn = cancellable((x1, x2) => x1 * x2, [2,4], 30);
+// setTimeout(cancelFn, cancelTimeMs);
+
+// The cancellation was scheduled to occur after a delay of cancelTimeMs (100ms), which happened after the execution of fn(2,4) at 30ms.
