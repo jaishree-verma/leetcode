5196feda7b3694da7df2f621ae1ade5df28f2f0f
jaishree-verma <jaishree.verma.work@gmail.com>
2025-11-14 16:02:36 +0530

    Python Basics

---
+++ Python/Lists/lists.py
@@ -0,0 +1,2 @@
+names = ['John', 'Mark', 'Bob', 'Sarah']
+print(names[2])   #index access in list 
---
+++ Python/Lists/listsmethod.py
@@ -0,0 +1,7 @@
+numbers = [5, 2, 1, 7, 4]
+numbers.append(10)     # append in last
+numbers.insert(0, 10)  # insert (index , object)
+print(numbers)
+# clear()-> remove all item
+# pop() -> remove last item
+# remove(value)-> remove element
