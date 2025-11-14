564c0a1dc3b0d1c27107ee11ab67e259df9c37d0
jaishree-verma <jaishree.verma.work@gmail.com>
2025-11-14 09:37:38 +0530

    JavaScript Basics

---
+++ JavaScript/Basics/intervalcancellation.js
@@ -0,0 +1,50 @@
+// // 2725
+
+// Example 
+// Input: fn = (x) => x * 2, args = [4], t = 35
+// Output: 
+// [
+//    {"time": 0, "returned": 8},
+//    {"time": 35, "returned": 8},
+//    {"time": 70, "returned": 8},
+//    {"time": 105, "returned": 8},
+//    {"time": 140, "returned": 8},
+//    {"time": 175, "returned": 8}
+// ]
+// Explanation: 
+// const cancelTimeMs = 190;
+// const cancelFn = cancellable((x) => x * 2, [4], 35);
+// setTimeout(cancelFn, cancelTimeMs);
+
+// Every 35ms, fn(4) is called. Until t=190ms, then it is cancelled.
+// 1st fn call is at 0ms. fn(4) returns 8.
+// 2nd fn call is at 35ms. fn(4) returns 8.
+// 3rd fn call is at 70ms. fn(4) returns 8.
+// 4th fn call is at 105ms. fn(4) returns 8.
+// 5th fn call is at 140ms. fn(4) returns 8.
+// 6th fn call is at 175ms. fn(4) returns 8.
+// Cancelled at 190ms
+
+// Example 
+// Input: fn = (x1, x2) => (x1 * x2), args = [2, 5], t = 30
+// Output: 
+// [
+//    {"time": 0, "returned": 10},
+//    {"time": 30, "returned": 10},
+//    {"time": 60, "returned": 10},
+//    {"time": 90, "returned": 10},
+//    {"time": 120, "returned": 10},
+//    {"time": 150, "returned": 10}
+// ]
+// Explanation: 
+// const cancelTimeMs = 165; 
+// const cancelFn = cancellable((x1, x2) => (x1 * x2), [2, 5], 30) 
+// setTimeout(cancelFn, cancelTimeMs)
+// Every 30ms, fn(2, 5) is called. Until t=165ms, then it is cancelled.
+// 1st fn call is at 0ms 
+// 2nd fn call is at 30ms 
+// 3rd fn call is at 60ms 
+// 4th fn call is at 90ms 
+// 5th fn call is at 120ms 
+// 6th fn call is at 150ms
+// Cancelled at 165ms
