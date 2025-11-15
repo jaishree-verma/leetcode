223f9a4a38651e44225b1db044ba55fe18fdd537
jaishree-verma <jaishree.verma.work@gmail.com>
2025-11-15 15:35:33 +0530

    Basics of Python

--- Python/Dictionaries/dict.py
+++ Python/Dictionaries/dict.py
@@ -3,16 +3,20 @@ customer = {
     "name": "Jaishree Verma",
     "country": "India",
 }
+# or add like this
+customer["birthdate"] = "Nov 16"
+print(customer["birthdate"])
 # each key should be unquie like real english dictionary no duplicacy
 print(type(customer))
 print(customer["name"])     # access value
 # print(customer["age"])      # error no key as age
 print(customer.get("name"))
-print(customer.get("age"))   # get return none not error since it means no value 
-print(customer.get("age","Nov"))   # but now when you use get with a value assign then it returns that value 
+# get return none not error since it means no value
+print(customer.get("age"))
+# but now when you use get with a value assign then it returns that value
+print(customer.get("age", "Nov"))
 
 
 # git add .
 # git commit -m "Basics of Python"
 # git push -u origin main
-
