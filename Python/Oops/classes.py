6f1fb1dc9f77e2c494c7ddaa19fed344f145b85e
jaishree-verma <jaishree.verma.work@gmail.com>
2025-11-17 18:35:42 +0530

    Basics of Python

--- Python/Oops/classes.py
+++ Python/Oops/classes.py
@@ -1,2 +1,15 @@
 # Classes in python
 # we use classes to use new types to model real concepts 
+# to define we use class + class_name and perform methods inside it
+# use uppercase (P for point) called as pascal naming convention for class and rest variables or function use lowercase
+class Point:
+    # block -> fucntions or methods that belong to point
+    def move(self):    # move is a func    # method 1 
+        print("move")
+    def draw(self):   # draw is a func     # method 1
+        print("draw")
+
+
+# now we create object with thse 2 point class methods
+# Object is a instance of a class
+# A class defined a blueprint for creating objects
