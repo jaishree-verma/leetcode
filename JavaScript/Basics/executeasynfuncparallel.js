72a7ed4c639a84291777e991ac0839cf39e7a304
jaishree-verma <jaishree.verma.work@gmail.com>
2025-11-18 14:14:37 +0530

    JavaScript Basics

---
+++ JavaScript/Basics/executeasynfuncparallel.js
@@ -0,0 +1,28 @@
+// // 2721
+
+// Example 
+// Input: functions = [
+//   () => new Promise(resolve => setTimeout(() => resolve(5), 200))
+// ]
+// Output: {"t": 200, "resolved": [5]}
+// Explanation: 
+// promiseAll(functions).then(console.log); // [5]
+// The single function was resolved at 200ms with a value of 5.
+
+// Example 
+// Input: functions = [
+//     () => new Promise(resolve => setTimeout(() => resolve(1), 200)), 
+//     () => new Promise((resolve, reject) => setTimeout(() => reject("Error"), 100))
+// ]
+// Output: {"t": 100, "rejected": "Error"}
+// Explanation: Since one of the promises rejected, the returned promise also rejected with the same error at the same time.
+
+// Example 
+// Input: functions = [
+//     () => new Promise(resolve => setTimeout(() => resolve(4), 50)), 
+//     () => new Promise(resolve => setTimeout(() => resolve(10), 150)), 
+//     () => new Promise(resolve => setTimeout(() => resolve(16), 100))
+// ]
+// Output: {"t": 150, "resolved": [4, 10, 16]}
+// Explanation: All the promises resolved with a value. The returned promise resolved when the last promise resolved.
+
