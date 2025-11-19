e7a7cddaf7460640b82919aea7ec861bf29fcfb4
jaishree-verma <jaishree.verma.work@gmail.com>
2025-11-19 11:37:05 +0530

    JavaScript Basics

--- JavaScript/Basics/isbojectempty.js
+++ JavaScript/Basics/isbojectempty.js
@@ -1,18 +1,20 @@
 // // 2727
 
 // Example 1:
-
 // Input: obj = {"x": 5, "y": 42}
 // Output: false
 // Explanation: The object has 2 key-value pairs so it is not empty.
-// Example 2:
 
+// Example 
 // Input: obj = {}
 // Output: true
 // Explanation: The object doesn't have any key-value pairs so it is empty.
-// Example 3:
 
+// Example 
 // Input: obj = [null, false, 0]
 // Output: false
 // Explanation: The array has 3 elements so it is not empty.
  
+var isEmpty = function(obj) {
+    return Object.keys(obj).length === 0;
+};
