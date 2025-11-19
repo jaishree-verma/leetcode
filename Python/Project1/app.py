6584aec75c7fdba0e2721077a69de76321faea5b
jaishree-verma <jaishree.verma.work@gmail.com>
2025-11-19 18:07:56 +0530

    Python

--- Python/Project1/app.py
+++ Python/Project1/app.py
@@ -1,4 +1,6 @@
 import openpyxl as xl
+# to add chart -> package = openpyxl, module = chart,   = Barchart and Refernce
+from openpyxl.chart import BarChart , Reference
 wb = xl.load_workbook(r'C:\leetcode\Python\Project1\transaction.xlsx')
 sheet = wb['Sheet1']
 cell = sheet['a1']
@@ -11,9 +13,23 @@ for row in range(2, sheet.max_row + 1):   # iterate through each row to get colu
     # print(row)
     cell = sheet.cell(row, 3)            # row,column = 3
     corrected_price = cell.value * 0.9       # multiply that column with 0.9
-    corrected_price_cell = sheet.cell(row,4) # now we add 4th column and store there in col 4
+    corrected_price_cell = sheet.cell(row,4) # now we add 4th column and store there in col 4 so that col 4 has value with formula ans 
     corrected_price_cell.value = corrected_price
     # print(cell.value)                        # we got values from 3rd column
 
+# we use refernce class to set range of values
+# we select sheet and select cells in row 2 to max row (4) available in that sheet
+# we want values in 4th column only
+values = Reference(sheet,
+          min_row=2,
+          max_row=sheet.max_row,
+          min_col=4,
+          max_4=4)
+
+# create instance of Barchart Class
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart,'e2')
 wb.save('transaction2.xlsx')
 # we create and save it in a new file
+# this creates another column with those calculated values
