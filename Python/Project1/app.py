72978ebc69aaf602f2250972311f8e221c674e99
jaishree-verma <jaishree.verma.work@gmail.com>
2025-11-19 22:17:46 +0530

    Python

--- Python/Project1/app.py
+++ Python/Project1/app.py
@@ -1,7 +1,7 @@
 import openpyxl as xl
 # to add chart -> package = openpyxl, module = chart,   = Barchart and Refernce
 from openpyxl.chart import BarChart , Reference
-wb = xl.load_workbook(r'C:\leetcode\Python\Project1\transaction.xlsx')
+wb = xl.load_workbook(r'C:\leetcode\Python\Project1\transaction.xlsx')          # file name 
 sheet = wb['Sheet1']
 cell = sheet['a1']
 # print(cell.value)
@@ -24,7 +24,7 @@ values = Reference(sheet,
           min_row=2,
           max_row=sheet.max_row,
           min_col=4,
-          max_4=4)
+          max_col=4)
 
 # create instance of Barchart Class
 chart = BarChart()
@@ -33,3 +33,31 @@ sheet.add_chart(chart,'e2')
 wb.save('transaction2.xlsx')
 # we create and save it in a new file
 # this creates another column with those calculated values
+
+
+
+
+
+# clean code 
+
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+def process_workbook(filename):
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
+
+    values = Reference(sheet,
+                    min_row=2,
+                    max_row=sheet.max_row,
+                    min_col=4,
+                    max_col=4)
+
+    chart = BarChart() 
+    chart.add_data(values)
+    sheet.add_chart(chart, 'E2')
+    wb.save(filename)
