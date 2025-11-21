0e480e044c6f2ac4e35e67b3259c2b145116bc67
jaishree-verma <jaishree.verma.work@gmail.com>
2025-11-21 09:46:40 +0530

    JavaScript Basics

--- JavaScript/Basics/arrayprototypelast.js
+++ JavaScript/Basics/arrayprototypelast.js
@@ -1,11 +1,19 @@
 // // 2619
-// Example 1:
 
+// Example 
 // Input: nums = [null, {}, 3]
 // Output: 3
 // Explanation: Calling nums.last() should return the last element: 3.
-// Example 2:
 
+// Example 
 // Input: nums = []
 // Output: -1
 // Explanation: Because there are no elements, return -1.
+
+Array.prototype.last = function() {
+  if (this.length === 0) {
+    return -1;
+  } else {
+    return this[this.length - 1];
+  }
+};
