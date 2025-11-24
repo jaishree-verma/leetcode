81edef8d03abc4e4e23f6e2140efabbcb6cb9709
jaishree-verma <jaishree.verma.work@gmail.com>
2025-11-24 18:33:13 +0530

    error avoid

--- Java/twosum.java
+++ Java/twosum.java
@@ -2,14 +2,20 @@
 // optimal approach is using Hash Map 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // create a hash map to store numbers
         Map<Integer,Integer> map = new HashMap<>();
         for ( int i = 0;i<nums.length;i++){
+            // calculate remainder of current number
             int remainder = target - nums[i];
+            // check if remainder is in hash map already 
             if (map.containsKey(remainder)){
+                // if yes return index and current no
                 return new int [] {map.get(remainder),i};
             }
+            // else add the current no and its index to hash map
             map.put(nums[i],i);
         }
+        // return empty array if no solution if found (worst case)
         return new int [] {};
     }
 }
