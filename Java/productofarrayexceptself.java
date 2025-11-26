978e1ef79e02a4717aed0bbb956070442d02d36a
jaishree-verma <jaishree.verma.work@gmail.com>
2025-11-26 18:57:08 +0530

    Java

---
+++ Java/productofarrayexceptself.java
@@ -0,0 +1,20 @@
+// 238
+
+// optimal approach to simple use prefix and post variable and store result in answer array
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        Arrays.fill(result,1);
+        int prefix = 1,post = 1;
+        for(int i = 0; i<nums.length;i++){
+            result[i]=prefix;
+            prefix = nums[i]*prefix;
+        }
+        for(int i = nums.length-1;i>=0;i--){
+            result[i] = result[i]*post;
+            post=post*nums[i];
+        }
+        return result;
+    }
+}
